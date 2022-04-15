25f066c945b7bf309b3677e538798ad2f443ea0c
palmorezm <zachary.palmore20@spsmail.cuny.edu>
2022-04-15 13:38:44 -0500

    Clean, Rinse, Repeat
    Created app2.R to test the data from app.R in a seperate user 'test' environment which worked. Hoorah! Tried funneling data for NYC into the functions from our_responsibility.R and either could not see them (blank map once) or did not have geometries to match so conversion to spatial dataframe was not happening. To solve I gathered new data with geoms we know work (those geoms of the counties_NY). This turned out well so far but there are small errors in the data types it looks like. "addCircleMarkers requires numeric values for lat/lon" is one and the other is "addCircleMarkers requires non-NULL longitude/latitude values." I might be missing something in the UI but I followed the process as best I could so I'm not sure yet what's happening there. I will continue playing with app2.R soon enough to see how the error occurs and hopefully (once its solved) prevent future errors like that from happening for those who use the functions/package.

--- api_access.R
+++ api_access.R
@@ -7,6 +7,7 @@ require(RSocrata)
 # An APP_TOKEN is needed to ensure these run
  
 # Modified Zip Code Tabulation Areas (Maintained by DOHMH)
+# Geometries: Unique for NYC
 # https://data.cityofnewyork.us/Health/Modified-Zip-Code-Tabulation-Areas-MODZCTA-/pri4-ifjk
 # Also sounds like Jason would really like to see this data displayed somehow 
 # See Git Notes on Homepage: https://github.com/jbryer/NYCFutureMap
@@ -19,6 +20,7 @@ MODZCTA <- read.socrata(
 )
 
 # NYC Open Data on Motor Vehicle Colisions
+# Geometries: Points in NYC
 # https://data.cityofnewyork.us/Public-Safety/Motor-Vehicle-Collisions-Crashes/h9gi-nx95
 MVColisions <- read.socrata(
   "https://data.cityofnewyork.us/resource/h9gi-nx95.json?$limit=10000", 
@@ -28,6 +30,7 @@ MVColisions <- read.socrata(
 )
 
 # NYPD Complaint Data Historic
+# Geometries: Points in NYC 
 # https://data.cityofnewyork.us/Public-Safety/NYPD-Complaint-Data-Historic/qgea-i56i
 Complaints <- read.socrata(
   "https://data.cityofnewyork.us/resource/qgea-i56i.json?$limit=10000", 
@@ -37,6 +40,7 @@ Complaints <- read.socrata(
 )
 
 # New York City Leading Causes of Death
+# Geometries: None
 # https://data.cityofnewyork.us/Health/New-York-City-Leading-Causes-of-Death/jb7j-dtam
 Deaths <- read.socrata(
   "https://data.cityofnewyork.us/resource/jb7j-dtam.json", 
@@ -46,6 +50,7 @@ Deaths <- read.socrata(
 )
 
 # Bus Routes 
+# Geometries: Lines in NYC
 # https://data.cityofnewyork.us/Transportation/Bus-Lanes-Local-Streets/ycrg-ses3
 Routes <- read.socrata(
   "https://data.cityofnewyork.us/resource/ycrg-ses3.json", 
@@ -53,6 +58,90 @@ Routes <- read.socrata(
   email     = Sys.getenv("PHDS_EMAIL"),
   password  = Sys.getenv("PHDS_PASSWORD")
 )
+
+# Income Class by Quantity of Tax Returns *1999 - 2014 
+# Geometries: Counties in NY state
+# https://data.ny.gov/Government-Finance/Income-Tax-Components-by-Size-of-Income-by-Place-o/5bb2-yb85
+Income <- read.socrata(
+  "https://data.ny.gov/resource/5bb2-yb85.json", 
+  app_token = Sys.getenv("APP_TOKEN"), 
+  email = Sys.getenv("PHDS_EMAIL"), 
+  password = Sys.getenv("PHDS_PASSWORD")
+)
+
+# Lottery locations in NY (all retailers who sell lotto products) *2022
+# Geometries: Points all over NY state
+# https://data.ny.gov/Government-Finance/NYS-Lottery-Retailers/2vvn-pdyi
+Lotto <- read.socrata(
+  "https://data.ny.gov/resource/2vvn-pdyi.json", 
+  app_token = Sys.getenv("APP_TOKEN"), 
+  email = Sys.getenv("PHDS_EMAIL"), 
+  password = Sys.getenv("PHDS_PASSWORD")
+)
+
+# Occupation data 
+# Geometries: Regions in NY state
+# https://data.ny.gov/Economic-Development/Occupational-Employment-and-Wage-Statistics/gkgz-nw24
+Occupations <- read.socrata(
+  "https://data.ny.gov/resource/gkgz-nw24.json", 
+  app_token = Sys.getenv("APP_TOKEN"), 
+  email = Sys.getenv("PHDS_EMAIL"), 
+  password = Sys.getenv("PHDS_PASSWORD")
+)
+
+# Electric Vehicles per County
+# Geometries: Counties in NY state
+# https://data.ny.gov/Transportation/Electric-Vehicles-per-County/uu25-czyc
+EVs <- read.socrata(
+  "https://data.ny.gov/resource/uu25-czyc.json", 
+  app_token = Sys.getenv("APP_TOKEN"), 
+  email = Sys.getenv("PHDS_EMAIL"), 
+  password = Sys.getenv("PHDS_PASSWORD")
+)
+
+# Vehicles, Snowmobile, and Boat Registrations
+# Geometries: Counties in NY state
+# https://data.ny.gov/Transportation/Vehicle-Snowmobile-and-Boat-Registrations/w4pv-hbkt
+Boats <- read.socrata(
+  "https://data.ny.gov/resource/w4pv-hbkt.json?$limit=10000", 
+  app_token = Sys.getenv("APP_TOKEN"), 
+  email = Sys.getenv("PHDS_EMAIL"), 
+  password = Sys.getenv("PHDS_PASSWORD")
+) 
+
+# Hate Crimes by County and Bias Type: Beginning 2010
+# Geometries: Counties in NY state
+# https://data.ny.gov/Public-Safety/Hate-Crimes-by-County-and-Bias-Type-Beginning-2010/6xda-q7ev
+HateCrimes <- read.socrata(
+  "https://data.ny.gov/resource/6xda-q7ev.json", 
+  app_token = Sys.getenv("APP_TOKEN"), 
+  email = Sys.getenv("PHDS_EMAIL"), 
+  password = Sys.getenv("PHDS_PASSWORD")
+) 
+
+# Recommended Fishing Rivers, Streams, Lakes and Ponds Map
+# Geometries: Points all over NY state
+# https://data.ny.gov/Recreation/Recommended-Fishing-Rivers-Streams-Lakes-and-Ponds/f4vj-p8y5
+BestFish <- read.socrata(
+  "https://data.ny.gov/resource/f4vj-p8y5.json", 
+  app_token = Sys.getenv("APP_TOKEN"), 
+  email = Sys.getenv("PHDS_EMAIL"), 
+  password = Sys.getenv("PHDS_PASSWORD")
+) 
+
+
+# Agricultural Fairs in New York State
+# Geometries: Points all over NY State
+# https://data.ny.gov/Recreation/Agricultural-Fairs-in-New-York-State/wcwd-s5vt
+AgFairs <- read.socrata(
+  "https://data.ny.gov/resource/wcwd-s5vt.json", 
+  app_token = Sys.getenv("APP_TOKEN"), 
+  email = Sys.getenv("PHDS_EMAIL"), 
+  password = Sys.getenv("PHDS_PASSWORD")
+) 
+
+save(Complaints, Income, Lotto, EVs, MODZCTA, MVColisions, 
+     Routes, Boats, HateCrimes, BestFish, AgFairs, file = "app2.rdata")
 
 # A potential addition should any of these not work
 # https://data.cityofnewyork.us/Transportation/TLC-Approved-LabCorp-Patient-Services-Drug-Test-Lo/hiik-hmf3
---
+++ app2.R
@@ -0,0 +1,257 @@
+# Packages-----
+library(leaflet)
+library(leaflet.extras)
+library(dplyr)
+library(shiny)
+library(readr)
+library(sf)
+
+
+
+#running the script that represents our eventual package
+source('our_responsibility.R') # eventually this would library(something)
+# * Ensure 'map_server' function is present * 
+# This is the only thing needed from source('our_responsibility.R') unless
+# Desired for running map_ui naming convention for main panel mapping
+load('app2.rdata')
+
+# 1. Pre-processing data into dataframes ------
+
+## Cleaning ----
+Boats <- Boats %>% 
+  filter(record_type == "BOAT") %>% 
+  group_by(county) %>% 
+  summarise(fill_value = n())
+HateCrimes <- HateCrimes %>% 
+  filter(year == "2020", 
+         crime_type == "Crimes Against Persons") %>% 
+  dplyr::select(county, year, total_victims) 
+HateCrimes$total_victims <- as.numeric(HateCrimes$total_victims)
+BestFish <- BestFish %>% 
+  dplyr::select(name, location.longitude, location.latitude, county, fish_spec, url) 
+AgFairs <- AgFairs %>% 
+  dplyr::select(georeference.coordinates, county, fair_name, url)
+# AgFairs$georeference.coordinates - Contains lat and long together (needs splitting)
+
+
+## Points ----
+points_complaints <- tibble(
+  #add lat/lon to a tibble for inputting to addMarkers() later
+  label = Complaints$pd_desc, 
+  long = Complaints$longitude, 
+  lat = Complaints$latitude
+)
+
+points_MVColisions <- tibble(
+  label = MVColisions$collision_id, 
+  long = MVColisions$longitude, 
+  lat = MVColisions$latitude
+)
+
+points_lotto <- tibble(
+  label = Lotto$name,
+  long = Lotto$longitude, 
+  lat = Lotto$latitude
+)
+
+points_fish <- tibble(
+  label = BestFish$name, 
+  long = BestFish$location.longitude, 
+  lat = BestFish$location.latitude
+)
+  
+# No binding necessary for these datasets
+
+## ----- Polygons -----
+
+# Load bundle saved from supported-polygons.R script
+load("counties_NY.rdata") # Might need/want geometries for MODZCTA? 
+  # Could also join with its own geoms to form spatial data frame? 
+
+## MODZCTA species 
+# class(as.numeric(MODZCTA$pop_est))
+# MODZCTA$pop_est <- as.numeric(MODZCTA$pop_est)
+# MODZCTA %>% 
+#   dplyr::select(MODZCTA, MODZCTA$the_geom.coordinates)
+# as_Spatial(MODZCTA) # Error in UseMethod("st_cast") : 
+#   no applicable method for 'st_cast' applied to an object of class "data.frame"
+# Special Note:
+# User should consider what geometries they need before mapping! Duh
+
+## Income classes from Returns -----
+Income$number_of_all_returns <- as.numeric(Income$number_of_all_returns) # Make number numeric for computation
+Income <- Income %>% 
+  filter(country == "United States", 
+         state == "New York") %>% 
+  group_by(tax_year, county, income_class, number_of_all_returns) %>%
+  summarise() 
+Income <- Income %>% 
+  filter(income_class == "Under 5,000", 
+         tax_year == "2014") %>% 
+  rename(NAME = county, 
+         fill_value = number_of_all_returns)   
+# Join and convert to Spatial Polygons
+Income <- counties_NY %>% 
+  left_join(Income, by = "NAME")
+Income <- as_Spatial(Income)
+
+## Electric Vehicles by County ----- * Clean DF Naming Later * 
+EVs$registration_class <- as.numeric(EVs$registration_class)
+EVs <- EVs %>%
+  rename(NAME = "county", 
+         fill_value = registration_class) %>% 
+  dplyr::select(NAME, fill_value) # NAME is capitalized here
+Evs <- counties_NY %>% # It is not capitalized here
+  left_join(EVs, by = "NAME") # Need exact matches for left_join 
+
+counties_NY$NAME <- toupper(counties_NY$NAME) # We capitalize names in spatial reference
+evs <- counties_NY %>% # New Uppercase NAME should match exactly with reference
+  left_join(EVs, by = "NAME") # Consider rename with EVs? 
+evs <- as_Spatial(evs) # Form Large Spatial DataFrame
+
+## Boat Registrations 
+Boats <- Boats %>% 
+  rename(
+   NAME = 'county')
+Boats <- counties_NY %>% 
+  left_join(Boats, by = 'NAME')
+Boats <- as_Spatial(Boats)
+
+## Hate Crimes in NY State 
+HateCrimes <- HateCrimes %>% 
+  transmute(NAME = toupper(county), 
+            year = as.numeric(year), 
+            fill_value = total_victims) 
+HateCrimes <- counties_NY %>% 
+  left_join(HateCrimes, by = "NAME")
+HateCrimes <- as_Spatial(HateCrimes)
+
+
+
+# 2. Create lists per tab/theme-----------------
+
+## for tab 1 -------------
+
+polys_income <- list(
+  list(
+    name = 'Income_class',
+    data = Income,
+    label = 'NAME',
+    fill = 'fill_value'
+  )
+)
+
+points_lotto <- list(
+  list(
+    name = 'Lotto',
+    data = Lotto,
+    long = 'longitude',
+    lat = 'latitude',
+    label = 'name'
+  )
+)
+
+#palette scaling for polygon fills
+pal_income_max <- as.numeric(max(Income@data$fill_value, na.rm = T)) # Improved!
+pal_income_min <- as.numeric(min(Income@data$fill_value, na.rm = T)) # Now works with missing values
+pal_income <- colorNumeric(c("RdYlGn"), pal_income_min:pal_income_max) # Woot!
+
+
+
+## for tab 2-------------
+
+polys_evs <- list(
+  list(
+    name = 'evs',
+    data = evs,
+    label = 'NAME',
+    fill = 'fill_value'
+  )
+)
+
+points_fish <- list(
+  list(
+    name = 'bestfish',
+    data = BestFish,
+    long = 'location.longitude',
+    lat = 'location.latitude',
+    label = 'fish_spec'
+  )
+)
+
+#palette scaling for polygon fills
+pal_evs_max <- as.numeric(max(evs@data$fill_value, na.rm = T)) # Improved!  
+pal_evs_min <- as.numeric(min(evs@data$fill_value, na.rm = T)) # Now works with missing values
+pal_evs <- colorNumeric(c("RdYlGn"), pal_evs_min:pal_evs_max) # Woot! 
+
+
+
+## for tab 3-------------
+
+polys_hatecrimes <- list(
+    name = 'hatecrimes',
+    data = HateCrimes,
+    label = 'NAME',
+    fill = 'fill_value'
+  )
+
+points_hatecrimeslotto <- list(
+  list(
+    name = 'Lotto',
+    data = Lotto,
+    long = 'longitude',
+    lat = 'latitude',
+    label = 'name'
+  )
+)
+
+#palette scaling for polygon fills
+pal_hatecrimes_max <- as.numeric(max(HateCrimes@data$fill_value, na.rm = T)) # Improved!  
+pal_hatecrimes_min <- as.numeric(min(HateCrimes@data$fill_value, na.rm = T)) # Now works with missing values
+pal_hatecrimes <- colorNumeric(c("RdYlGn"), pal_hatecrimes_min:pal_hatecrimes_max) # Woot! 
+
+
+
+# 3. Set ui/layout --------
+ui <- fluidPage(
+  titlePanel("Package Test Map 2"),
+  
+  # sidebarLayout(
+  #     sidebarPanel(
+  #     ),
+  
+  mainPanel(
+    tabsetPanel(
+      tabPanel('Income & Lotto', map_UI('income')),
+      tabPanel('EVs and Fish', map_UI('fishevs')),
+      tabPanel('Hate Crimes', map_UI('hatecrimes'))
+    )
+    
+  )
+)
+
+
+
+server <- function(input, output) {
+# 4. Create 1 map_server per theme/tab ------
+
+  map_server("income", 
+             polygons = polys_income,
+             points = points_lotto,
+             pal = pal_income
+  ) 
+  map_server("fishevs", 
+             polygons = polys_evs,
+             points = points_fish,
+             pal = pal_evs
+  ) 
+  map_server("hatecrimes", 
+             polygons = polys_hatecrimes,
+             points = points_hatecrimeslotto,
+             pal = pal_hatecrimes
+  ) 
+}
+
+
+# Run the application 
+shinyApp(ui = ui, server = server)
